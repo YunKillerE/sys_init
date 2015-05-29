a736928426cf136be84040b6c91d227d35c85d91
jimmy.liu <liujmsunits@hotmail.com>
2015-05-30 05:19:14 +0800

    modified the auto login shell

--- function.sh
+++ function.sh
@@ -4,11 +4,11 @@
 # Author: $liujmsunits@hotmail.com
 # Create Date: 2015-05-06 22:15
 # Modify Author: liujmsunits@hotmail.com
-# Modify Date: 2015-05-10 23:43
+# Modify Date: 2015-05-30 05:19
 # Function: 
 #***************************************************************#
+pass="r00tadmin"
 auto_ssh_copy_id(){
-#	pass="root"
 	expect -c "set timeout 10;
     	spawn ssh-copy-id $1;
   		expect *assword:*;
@@ -16,24 +16,14 @@ auto_ssh_copy_id(){
 		interact;";
 }
 
-#auto_ssh_copy_id 192.168.218.199
+auto_ssh_copy_id 192.168.218.199
 
 
 auto_login_ssh(){
 	expect -c "set timeout -1;
 		spawn -noecho ssh -o StrictHostKeyChecking=no $2 ${@:3};
 		expect *assword:*;
-		send -- $1\r;
+		send -- $pass\r;
 		interact;";
 }
-auto_login_ssh r00tadmin client "ifconfig eth0 && cat /root/.ssh/id_rsa"
-
-
-
-
-
-
-
-
-
-
+auto_login_ssh client "ifconfig eth0 && cat /root/.ssh/id_rsa"
